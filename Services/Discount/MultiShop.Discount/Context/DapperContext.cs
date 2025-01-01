4169c60c68c80f31fb68a59ea7351f166b6f70d2
BurakAyan97 <burakayan97@outlook.com>
2025-01-01 23:13:52 +0300

    discount API Created

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-S2C7UGO;initial catalog=MultiShopCatalogDb;integrated security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
