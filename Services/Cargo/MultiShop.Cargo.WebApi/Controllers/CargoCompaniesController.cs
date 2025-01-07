2c611af90d3ebbbff6075f9fcfc2d0425530fc61
BurakAyan97 <burakayan97@outlook.com>
2025-01-07 23:07:50 +0300

    authorize updated

--- Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompaniesController.cs
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.Cargo.Business.Abstract;
 using MultiShop.Cargo.DtoLayer.Dtos.CargoCompanyDtos;
